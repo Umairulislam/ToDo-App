49605931d127f6b40759cf861d85a8fdef4e38b4
umair <umairulislam09@gmail.com>
2020-07-28 11:34:45 +0500

    ToDo app

---
+++ app.js
@@ -0,0 +1,42 @@
+var list = document.getElementById("list");
+
+function addTodo() {
+    var todo_item = document.getElementById("todo-item")
+
+
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todo_item.value)
+    li.appendChild(liText)
+
+    //dlt btn
+    var delBtn = document.createElement("button")
+    var delText = document.createTextNode("DELETE")
+    delBtn.setAttribute("class","btn2")
+    delBtn.setAttribute("onclick","deleteItem(this)")
+    delBtn.appendChild(delText)
+
+
+    //edit btn
+    var editBtn = document.createElement("button")
+    var editText = document.createTextNode("EDIT")
+    editBtn.setAttribute("class","btn3")
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("onclick","editItem(this)")
+
+    li.appendChild(delBtn)
+    li.appendChild(editBtn)
+
+    list.appendChild(li)
+
+    todo_item.value = ""
+}
+
+
+function deleteItem(e){
+    e.parentNode.remove()
+}
+
+function editItem(e) {
+    var val = prompt("Enter updated value",e.paraentNode)
+    e.parentNode.firstChild.nodeValue = val;
+}
